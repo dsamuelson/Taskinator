de6dce84fd5bd4872f7444667ab9816f68ee9103
David Samuelson <linksn.1fan@gmail.com>
2022-02-08 13:55:38 -0700

    add name and type to tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
 let formEL = document.querySelector("#task-form");
-let tasksToDo = document.querySelector("#tasks-to-do");
+let tasksToDoEL = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
-    let taskItemEl = document.createElement("li");
-    taskItemEl.textContent = "A new task has been added";
-    taskItemEl.className = "task-item";
-    tasksToDo.appendChild(taskItemEl);
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+    let listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    tasksToDoEL.appendChild(listItemEl);
 };
 
 formEL.addEventListener("submit", createTaskHandler);
