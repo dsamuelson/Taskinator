52786dce3a244cdc5162a4c74eab54c6d802b918
David Samuelson <linksn.1fan@gmail.com>
2022-02-08 14:06:48 -0700

    refactored code to be more readable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,26 @@
 let formEL = document.querySelector("#task-form");
 let tasksToDoEL = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     event.preventDefault();
     let taskNameInput = document.querySelector("input[name='task-name']").value;
     let taskTypeInput = document.querySelector("select[name='task-type']").value;
+    let taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function (taskDataObj) {
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     let taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
     tasksToDoEL.appendChild(listItemEl);
 };
 
-formEL.addEventListener("submit", createTaskHandler);
+formEL.addEventListener("submit", taskFormHandler);
+
